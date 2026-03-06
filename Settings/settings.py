c394e819a3c1d3a6589648126bf17355802fd3a8
A1ikabar <148199192+A1ikabar@users.noreply.github.com>
2026-03-06 23:00:31 +0300

    Better Module Structure

--- Settings/settings.py
+++ Settings/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'web_labs.urls'
+ROOT_URLCONF = 'Settings.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'web_labs.wsgi.application'
+WSGI_APPLICATION = 'Settings.wsgi.application'
 
 
 # Database
